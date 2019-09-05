4c13c8a8c95bee5226d95dde97c5379cf165fe8e
Ian Candy <iancandysemail@gmail.com>
2019-09-05 16:16:08 -0400

    rename files

---
+++ conversions_test.rb
@@ -0,0 +1,17 @@
+require_relative './conversions.rb'
+
+puts "Given 0, it should return 0.0"
+answer = ounces_to_grams(0)
+if answer == 0.0
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
+
+puts "Given 1, it should return 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
